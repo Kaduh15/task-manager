e30acb600251748fe7f8ed58c8020f51493154b3
Kadu <kadu.silva2014@gmail.com>
2024-08-26 15:55:32 -0300

    Feat: config prisma ORM

---
+++ apps/api/src/config/database/prisma/migrations/20240826170741_create_task_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "tasks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -3,5 +3,7 @@ import z from 'zod'
 export const env = z
   .object({
     PORT: z.coerce.number().default(3000),
+    DATABASE_URL: z.string().url().default('file:./dev.db'),
+    JWT_SECRET: z.string().default('secret'),
   })
   .parse(process.env)
