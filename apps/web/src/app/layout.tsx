182b130fc35922f39be205b4957f0dfaca2b6ce0
Kadu <kadu.silva2014@gmail.com>
2024-08-27 09:13:18 -0300

    setup app web

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+export const metadata: Metadata = {
+  title: 'Gerenciador de Tarefas',
+  description: 'Gerencie suas tarefas de maneira simples e eficiente.',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={cn(
+          'bg-background min-h-screen font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
